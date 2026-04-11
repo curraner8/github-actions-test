1ebc68c6067e7b5ce50809cc4517fa81300ad567
Seán Currán <122413962@umail.ucc.ie>
2026-04-11 16:18:05 +0100

    remove files

--- jd.java
+++
@@ -1,7 +0,0 @@
-// VULNERABLE: Direct readObject call
-public void deserialize(byte[] data) throws Exception {
-    ObjectInputStream ois = new ObjectInputStream(
-        new ByteArrayInputStream(data)
-    );
-    Object obj = ois.readObject(); // B4 vulnerability
-}
--- safe.java
+++
@@ -1,4 +0,0 @@
-func pingHandler(ip string) {
-    cmd := exec.Command("ping", "-c", "4", ip) // Safe
-    cmd.Run()
-}
